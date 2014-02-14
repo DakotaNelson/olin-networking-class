a02f0cc572dba91adbc4ac3f07404e2c0038f528
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-02-14 00:50:35 -0500

    Added super basic skeleton structure to chatroom server and clients.

--- chatroom/chatroom_server.py
+++ chatroom/chatroom_server.py
@@ -2,3 +2,7 @@ import UDP_Server as Server
 
 serv = Server.UDP_Server() # using default ip 127.0.0.1 and port 5280
 rooms = {}
+
+while True:
+    serv.get_data() # this is a placeholder for now. Will return one message
+    # update users, echo/route messages to correct users, etc.
