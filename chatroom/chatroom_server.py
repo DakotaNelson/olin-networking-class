32722eb4556c5aea092bf7c016fb77234afee5d5
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-02-18 14:43:27 -0500

    Removed tabs.

--- chatroom/chatroom_server.py
+++ chatroom/chatroom_server.py
@@ -7,23 +7,23 @@ while True:
     current_message = serv.returnData(False) #this will return one packet from the server
     # packet will be in the form [source_IP,source_port,msg]
     # returnData(wait,timeout=None)
-    # update users, echo/route messages to correct users, etc.  
+    # update users, echo/route messages to correct users, etc.
 
     if (current_message[2][0] == "\\"):
-    	# message contains special command, do not retransmit.
-    	pass
-    	# need to parse special command here
+    # message contains special command, do not retransmit.
+      pass
+      # need to parse special command here
     else:
-	    for room in rooms.values:
-	    	for users in room:
-	    		if current_message[0] in users:
-	    			# sender is in chatroom room
-	    			echoMessages(room, users)
-	    		else:
-	    			# sender not in chatroom, no need to retransmit
-	    			pass
+      for room in rooms.values:
+        for users in room:
+          if current_message[0] in users:
+            # sender is in chatroom room
+            echoMessages(room, users)
+          else:
+            # sender not in chatroom, no need to retransmit
+            pass
 
-    
+
 
 def echoMessages(userList,incMsg):
     for room in userList:
