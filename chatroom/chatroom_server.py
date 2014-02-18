383b50eb80a1fb92b460365a45b9c6abebcc398e
promnius <Kyle.Mayer@students.olin.edu>
2014-02-18 14:36:46 -0500

    added code for determining which room a client is in

--- chatroom/chatroom_server.py
+++ chatroom/chatroom_server.py
@@ -4,18 +4,28 @@ serv = Server.UDP_Server() # using default ip 127.0.0.1 and port 5280
 rooms = {}
 
 while True:
-    serv.returnData(False) #this will return one packet from the server
+    current_message = serv.returnData(False) #this will return one packet from the server
     # packet will be in the form [source_IP,source_port,msg]
     # returnData(wait,timeout=None)
-    # see UDP_Server.py
+    # update users, echo/route messages to correct users, etc.  
 
-    # here: update users, echo/route messages to correct users, etc.
-
-def detRoom(msg):
-    for room in rooms.keys:
-        for user in rooms[room]:
-            if user[2] == msg[1] and user[3] == msg[2]:
-                return room
-    return false
+    if (current_message[2][0] == "\\"):
+    	# message contains special command, do not retransmit.
+    	pass
+    	# need to parse special command here
+    else:
+	    for room in rooms.values:
+	    	for users in room:
+	    		if current_message[0] in users:
+	    			# sender is in chatroom room
+	    			echoMessages(room, users)
+	    		else:
+	    			# sender not in chatroom, no need to retransmit
+	    			pass
 
     
+
+def echoMessages(userList,incMsg):
+    for room in userList:
+        for userNum in userList[room]:
+            serv.sendMessage(userList[room[userNum[1]]],userList[room[userNum[2]]],incMsg)
