03e089e7f7633d7e7e5559a4b9f55e1a1e6e038a
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-02-18 15:10:10 -0500

    Cleaning things up.

--- chatroom/chatroom_server.py
+++ chatroom/chatroom_server.py
@@ -4,7 +4,7 @@ serv = Server.UDP_Server() # using default ip 127.0.0.1 and port 5280
 rooms = {}
 
 while True:
-    current_message = serv.returnData(False) #this will return one packet from the server
+    current_message = serv.returnData(False) #this will return one packet from server
     # packet will be in the form [source_IP,source_port,msg]
     # returnData(wait,timeout=None)
     # update users, echo/route messages to correct users, etc.
