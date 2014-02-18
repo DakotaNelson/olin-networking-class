40390e6819d560666fcdc686fb96b83f84fa4726
Ben Tang <btang1@babson.edu>
2014-02-18 02:44:37 -0500

    Added echoMessages
    Added echoMessages to echo a message to every user in every room

--- chatroom/chatroom_server.py
+++ chatroom/chatroom_server.py
@@ -7,4 +7,10 @@ while True:
     serv.returnData(False) #this will return one packet from the server
     # packet will be in the form [source_IP,source_port,msg]
     # returnData(wait,timeout=None)
-    # update users, echo/route messages to correct users, etc.
+    # update users, echo/route messages to correct users, etc.    
+    
+
+def echoMessages(userList,incMsg):
+    for room in userList:
+        for userNum in userList[room]:
+            serv.sendMessage(userList[room[userNum[1]]],userList[room[userNum[2]]],incMsg)
