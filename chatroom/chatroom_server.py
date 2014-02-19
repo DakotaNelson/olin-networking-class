d8423d4fce8b8d874a4599b4905260d0988c0805
evarady <ezra.varady@students.olin.edu>
2014-02-18 16:12:15 -1000

    added stuff to allow for joining and leaving chatrooms

--- chatroom/chatroom_server.py
+++ chatroom/chatroom_server.py
@@ -9,28 +9,33 @@ while True:
     # returnData(wait,timeout=None)
     # update users, echo/route messages to correct users, etc.
 
-    if (current_message[2][0] == "\\"):
+    if (current_message[2][0] =='\\'):
     # message contains special command, do not retransmit.
-      pass
+        messageSplit = current_message[2].split(' ')
+        if '\\join' in current_message[2]:
+            if messageSplit[0]=='\\join' and messageSplit[1] != None:
+                if messageSplit[2] == None:
+                    messageSplit.append(current_message[0])
+                if messageSplit[1] in rooms.keys:
+                    rooms[messageSplit[1]].append([messageSplit[2],current_message[0],current_message[1])
+                else:
+                    rooms[messageSplit[1]]=[messageSplit[2],current_message[0],current_message[1]]
+            else: serv.sendMessage(current_message[0],current_message[1],'malformed join attempt')
+        if '\\leave' in message[2]:
+            if messageSplit[0]=='\\leave' and messageSplit[1] != None:
+                if room=detRoom(current_message)!=False:
+                    rooms[room] = [x for x in rooms[room] if x[1]!=current_message[0] and x[2]!=current_message[1]]
+                else: serv.sendMessage(current_message[0],current_message[1],'invalid room')
+            else: serv.sendMessage(current_message[0],current_message[1],'no room')
       # need to parse special command here
     else:
-      for room in rooms.values:
-        for users in room:
-          if current_message[0] in users:
-            # sender is in chatroom room
-            echoMessages(room, users)
-          else:
-            # sender not in chatroom, no need to retransmit
-            pass
-
-def echoMessages(userList,incMsg):
-    for room in userList:
-        for userNum in userList[room]:
-            serv.sendMessage(userList[room[userNum[1]]],userList[room[userNum[2]]],incMsg)
-
+        if room=detRoom(current_message)!=False:
+            for user in rooms[room[0]]:
+                serv.sendMessage(user[1],user[2],room[1]+':'+current_message[2])
+    
 def detRoom(msg):
     for room in rooms.keys:
         for user in rooms[room]:
             if user[2] == msg[1] and user[3] == msg[2]:
-                return room
-    return false
+                return [room, user[0]]
+    return False
