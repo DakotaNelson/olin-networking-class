10a211ed83cd02ce131838052e22514fb6a2a468
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-03-20 12:06:46 -0400

    Minor modifications to the router code - assigned interior address.

--- morse_code/morse_router.py
+++ morse_code/morse_router.py
@@ -4,8 +4,10 @@ import morse_ethernet
 lan = morse_socket(lan.AF_INET,lan.SOCK_DGRAM)
 wan = morse_ethernet()
 
-lan.bind([lanip,lanport]) # these still need to be determined
+lan.bind(["E.EE",80]) # this is universal among all E team routers
 wan.bind([wanip,wanport])
+# Since we're doing all broadcasts, this may not be necessary
+# (i.e. only if we want to address the router itself for some reason)
 
 lan.settimeout(0.1)
 wan.settimeout(0.1)
@@ -17,6 +19,7 @@ while True:
         #print("No LAN messages.")
     if msg is not None:
         # determine recipient of the message here
+        # if it's to a groupcode other than E, route it out
         wan.sendto(msg,[destip,destport])
 
     try:
@@ -25,4 +28,5 @@ while True:
         #print("No WAN messages.")
     if msg is not None:
         # determine recipient of the message here
+        # if it's to E, route it in to the network
         lan.sendto(msg,[destip,destport])
