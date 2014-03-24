fac23a235471952529f627f7e4ddaccd315f81d8
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-03-24 13:28:19 -0400

    Take two.

---
+++ applications/netcam/netcam.py
@@ -0,0 +1,6 @@
+import picamera
+
+# Basically: take a picture with the raspberry pi camera and send it across the
+# network. Now with only two days of latency!
+
+# Learning objectives: encoding, compression, use of hilariously slow networks.
