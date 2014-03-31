f0f0ed33b74059334d2d5001be0f2ad80dc50005
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-03-30 20:16:18 -0400

    woo tabs

--- morse_code/morse_router.py
+++ morse_code/morse_router.py
@@ -21,7 +21,7 @@ with socket(AF_INET, SOCK_DGRAM) as wan:
             #print("No LAN messages.")
         if msg is not None:
             print("Got a message from the LAN.")
-	    print([msg,addr])
+            print([msg,addr])
             destip = address[0]
             destip = destip.split('.') # split the IP into its component octets
             #destport = address[1]
@@ -29,9 +29,9 @@ with socket(AF_INET, SOCK_DGRAM) as wan:
             if str(destip[2]) is not '69':
                 destmac = wanNAT[destip[3]]
                 destport = portLookup[destip[2]]
-	        print("Sending message to:")
-		print(msg)
-		print([destmac,destport])
+            print("Sending message to:")
+            print(msg)
+            print([destmac,destport])
                 wan.sendto(msg,[destmac,destport])
 
         try:
@@ -42,14 +42,14 @@ with socket(AF_INET, SOCK_DGRAM) as wan:
         if msg is not None:
             # determine recipient of the message
             # if it's to E, route it into the network
-	    print("Got a message from the WAN.")
-	    print([msg,addr])
+            print("Got a message from the WAN.")
+            print([msg,addr])
             destip = addr[0]
             destport = addr[1]
             destip = destip.split('.') # split the IP into its component bytes
             if str(destip[2]) is '69':
                 destmac = lanNAT[destip[3]]
-		print("Sending message to:")
-		print(msg)
-		print([destmac,11])
+                print("Sending message to:")
+                print(msg)
+                print([destmac,11])
                 lan.sendto(msg,[destmac,11])
