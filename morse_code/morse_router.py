2ce32d5279b85114853ba2daea8b2d52e2cdb2d5
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-04-03 17:12:33 -0400

    Batch indenting in vim makes me sad.

--- morse_code/morse_router.py
+++ morse_code/morse_router.py
@@ -12,51 +12,51 @@ wan = morse_socket(morse_socket.AF_INET,morse_socket.SOCK_DGRAM)
 
 
 #with socket(AF_INET, SOCK_DGRAM) as wan:
-wan.settimeout(0.1)
-lan.settimeout(0.1)
+   wan.settimeout(0.1)
+   lan.settimeout(0.1)
 
 while True:
-try:
-msg,addr = lan.recvfrom()
-except timeout:
-print("No LAN messages.")
+    try:
+        msg,addr = lan.recvfrom()
+    except timeout:
+        print("No LAN messages.")
 
-if msg is not None:
-print("Got a message from the LAN.")
-print([msg,addr])
-destip = address[0]
-destip = destip.split('.') # split the IP into its component octets
-#destport = address[1]
-print(address)
-groupCode = str(destip[-2])
-deviceCode = str(destip[-1])
-if groupcode is not '69':
-    destmac = wanNAT[deviceCode]
-    destport = portLookup[groupCode]
-    print("Sending message to:")
-    print(msg)
-    print([destmac,destport])
-    wan.sendto(bytearray(msg),[destmac,destport])
+    if msg is not None:
+        print("Got a message from the LAN.")
+        print([msg,addr])
+        destip = address[0]
+        destip = destip.split('.') # split the IP into its component octets
+        #destport = address[1]
+        print(address)
+        groupCode = str(destip[-2])
+        deviceCode = str(destip[-1])
+        if groupcode is not '69':
+            destmac = wanNAT[deviceCode]
+            destport = portLookup[groupCode]
+            print("Sending message to:")
+            print(msg)
+            print([destmac,destport])
+            wan.sendto(bytearray(msg),[destmac,destport])
 
-try:
-bytearray_msg, addr = wan.recvfrom(1024)
-msg = bytearray_msg.decode("UTF-8")
-except timeout:
-print("No WAN messages.")
+    try:
+        bytearray_msg, addr = wan.recvfrom(1024)
+        msg = bytearray_msg.decode("UTF-8")
+    except timeout:
+        print("No WAN messages.")
 
-if msg is not None:
-# determine recipient of the message
-# if it's to E, route it into the network
-print("Got a message from the WAN.")
-print([msg,addr])
-destip = addr[0]
-destport = addr[1]
-destip = destip.split('.') # split the IP into its component bytes
-groupCode = str(destip[-2])
-deviceCode = str(destip[-1])
-if groupCode is '69':
-    destmac = lanNAT[deviceCode]
-    print("Sending message to:")
-    print(msg)
-    print([destmac,11])
-    lan.sendto(bytearray(msg),[destmac,11])
+    if msg is not None:
+    # determine recipient of the message
+    # if it's to E, route it into the network
+        print("Got a message from the WAN.")
+        print([msg,addr])
+        destip = addr[0]
+        destport = addr[1]
+        destip = destip.split('.') # split the IP into its component bytes
+        groupCode = str(destip[-2])
+        deviceCode = str(destip[-1])
+        if groupCode is '69':
+            destmac = lanNAT[deviceCode]
+            print("Sending message to:")
+            print(msg)
+            print([destmac,11])
+            lan.sendto(bytearray(msg),[destmac,11])
