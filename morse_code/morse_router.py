65ab10bf8500a95e101baae35f10d36ab682e8b9
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-04-03 17:09:28 -0400

    Converted router to use GPIO ports on both sides instead of Ethernet port for WAN.

--- morse_code/morse_router.py
+++ morse_code/morse_router.py
@@ -1,5 +1,4 @@
 import morse_socket
-import CN_Sockets
 
 lanNAT = {'83':'ES','73':'EI','69':'EE','84':'ET'}
 wanNAT = {'T2':'51','T3':'52','IR':'82','T1':'50','II':'73','IN':'78','ID':'68'}
@@ -8,54 +7,56 @@ wanNAT = {'T2':'51','T3':'52','IR':'82','T1':'50','II':'73','IN':'78','ID':'68'}
 portLookup = {'84':'A','69':'11','73':'E'}
 
 lan = morse_socket(morse_socket.AF_INET,morse_socket.SOCK_DGRAM)
-wan, AF_INET, SOCK_DGRAM, timeout = CN_Sockets.socket, CN_Sockets.AF_INET, CN_Sockets.SOCK_DGRAM, CN_Sockets.timeout
+wan = morse_socket(morse_socket.AF_INET,morse_socket.SOCK_DGRAM)
+#wan, AF_INET, SOCK_DGRAM, timeout = CN_Sockets.socket, CN_Sockets.AF_INET, CN_Sockets.SOCK_DGRAM, CN_Sockets.timeout
 
-with socket(AF_INET, SOCK_DGRAM) as wan:
-    wan.settimeout(0.1)
-    lan.settimeout(0.1)
 
-    while True:
-        try:
-            msg,addr = lan.recvfrom()
-        except timeout:
-            print("No LAN messages.")
+#with socket(AF_INET, SOCK_DGRAM) as wan:
+wan.settimeout(0.1)
+lan.settimeout(0.1)
 
-        if msg is not None:
-            print("Got a message from the LAN.")
-            print([msg,addr])
-            destip = address[0]
-            destip = destip.split('.') # split the IP into its component octets
-            #destport = address[1]
-            print(address)
-            groupCode = str(destip[-2])
-            deviceCode = str(destip[-1])
-            if groupcode is not '69':
-                destmac = wanNAT[deviceCode]
-                destport = portLookup[groupCode]
-                print("Sending message to:")
-                print(msg)
-                print([destmac,destport])
-                wan.sendto(bytearray(msg),[destmac,destport])
+while True:
+try:
+msg,addr = lan.recvfrom()
+except timeout:
+print("No LAN messages.")
 
-        try:
-            bytearray_msg, addr = wan.recvfrom(1024)
-            msg = bytearray_msg.decode("UTF-8")
-        except timeout:
-            print("No WAN messages.")
+if msg is not None:
+print("Got a message from the LAN.")
+print([msg,addr])
+destip = address[0]
+destip = destip.split('.') # split the IP into its component octets
+#destport = address[1]
+print(address)
+groupCode = str(destip[-2])
+deviceCode = str(destip[-1])
+if groupcode is not '69':
+    destmac = wanNAT[deviceCode]
+    destport = portLookup[groupCode]
+    print("Sending message to:")
+    print(msg)
+    print([destmac,destport])
+    wan.sendto(bytearray(msg),[destmac,destport])
 
-        if msg is not None:
-            # determine recipient of the message
-            # if it's to E, route it into the network
-            print("Got a message from the WAN.")
-            print([msg,addr])
-            destip = addr[0]
-            destport = addr[1]
-            destip = destip.split('.') # split the IP into its component bytes
-            groupCode = str(destip[-2])
-            deviceCode = str(destip[-1])
-            if groupCode is '69':
-                destmac = lanNAT[deviceCode]
-                print("Sending message to:")
-                print(msg)
-                print([destmac,11])
-                lan.sendto(bytearray(msg),[destmac,11])
+try:
+bytearray_msg, addr = wan.recvfrom(1024)
+msg = bytearray_msg.decode("UTF-8")
+except timeout:
+print("No WAN messages.")
+
+if msg is not None:
+# determine recipient of the message
+# if it's to E, route it into the network
+print("Got a message from the WAN.")
+print([msg,addr])
+destip = addr[0]
+destport = addr[1]
+destip = destip.split('.') # split the IP into its component bytes
+groupCode = str(destip[-2])
+deviceCode = str(destip[-1])
+if groupCode is '69':
+    destmac = lanNAT[deviceCode]
+    print("Sending message to:")
+    print(msg)
+    print([destmac,11])
+    lan.sendto(bytearray(msg),[destmac,11])
