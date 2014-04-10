b8cece43a682ae1e19a97acfde7124867404be02
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-04-10 18:47:09 -0400

    Netcam now converts captured images to greyscale numpy arrays

--- applications/netcam/netcam.py
+++ applications/netcam/netcam.py
@@ -1,5 +1,6 @@
 import picamera
 import time
+import numpy as np
 
 # Basically: take a picture with the raspberry pi camera and send it across the
 # morse_code network. Now with only two days of latency!
@@ -7,9 +8,24 @@ import time
 # Learning objectives: encoding, compression, use of hilariously slow networks.
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (64,64)
+    size = 64 # get a 64x64 image
+    camera.resolution = (size,size)
+    camera.color_effects = (128,128) # make the camera capture in black and white
 
     camera.start_preview()
     time.sleep(5)
-    camera.capture('test.jpg')
+    camera.capture('test.data', 'yuv')
     camera.stop_preview()
+
+    img = open("test.jpg","rb")
+    Y = np.fromfile(img, dtype=np.uint8, count=size*size)\
+            .reshape((size,size)).astype(np.float)
+
+    print(Y)
+
+    #data = img.read(16)
+    #while True:
+        #data = img.read(16)
+        #if not data:
+            #break
+        #print(repr(data))
