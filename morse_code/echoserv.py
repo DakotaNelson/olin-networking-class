11a7ae828ebabd53181525c0c9dbfbaa59883836
ezra <ezra.varady@students.olin.edu>
2014-04-11 07:47:58 +0000

    printed address before asking for target address

--- morse_code/echoserv.py
+++ morse_code/echoserv.py
@@ -1,9 +1,9 @@
 from morse_socket import morse_socket
 sock = morse_socket(morse_socket.AF_INET,morse_socket.SOCK_DGRAM)
 sock.network.off()
+sock.bind(["EI",11])
 mac = input("Enter the target server's two character MAC:")
 mac = mac.upper()
-sock.bind(["EI",11])
 while True:
     msg = input("Enter a message to send to the server, composed of only alphanumerics... and no spaces or punctuation:")
     print(msg)
